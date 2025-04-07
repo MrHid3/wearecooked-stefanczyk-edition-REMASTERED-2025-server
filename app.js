bfe899854a75ce9994bb079676d91f4a2d8e1161
MrHid3 <gkornas@proton.me>
2025-04-07 19:02:51 +0200

    epico servcer

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const cors = require('cors');
 const express = require('express');
-const data = require("./data/avatars.json")
-
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+
+app.use(express.json())
+// app.use(express.urlencoded({ extended: true }));
+
 app.set('port', port);
 
-app.get("/data", function (req, res) {
-    res.json(data)
+let people = [{
+        name: "John",
+        password: "Doe"
+    }]
+
+app.post("/sendPeople", function (req, res) {
+    console.log(people)
+    if(req.body.name == undefined || req.body.name.length == 0){
+        res.status(400).send("NONAME")
+        return
+    }
+    if(req.body.password == undefined || req.body.password.length == 0){
+        res.status(400).send("NOPASSWORD")
+        return
+    }
+    for(let i = 0; i < people.length; i++){
+        if(people[i].name === req.body.name){
+            res.status(400).send("USEREXISTS")
+            return;
+        }
+    }
+    people.push({
+        name: req.body.name,
+        password: req.body.password
+    })
+    res.status(200).send("OK")
+})
+
+app.get("/", (req, res) => {
+    res.send("siema");
+})
+
+app.post("/getPeople", function (req, res) {
+    console.log(people)
+    res.send(JSON.stringify(people));
 })
 
 app.listen(port, function () {
